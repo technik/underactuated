fec8d80a5717ee03c4076e04d42f831673f40034
Carmelo <carferna@microsoft.com>
2024-03-24 19:13:18 -0700

    NN fixes

--- ml/nn/FullyConnectedNN.h
+++ ml/nn/FullyConnectedNN.h
@@ -1,17 +1,17 @@
 #pragma once
 
 #include "layer.h"
+#include <math/noise.h>
 
 namespace nn
 {
-    template<class S>
     class FullyConnectedNN
     {
     public:
-        using Scalar = S;
-        using Layer = FullyConnectedLayer<Scalar>;
+        using Scalar = float;
+        using Layer = FullyConnectedLayer;
         using Matrix = Layer::Matrix;
-        using ActivationFn = ReLu<Scalar>;
+        using ActivationFn = ReLu;
 
         std::vector<Layer> m_layers;
         ActivationFn m_activationFn;
@@ -20,41 +20,79 @@ namespace nn
         FullyConnectedNN(size_t numHiddenLayers, size_t inputWidth, size_t hiddenLayerWidth, size_t outputWidth)
         {
             // Init weight layers.
-            // Each layer's input size is extended by one to accomodate a weight
             m_layers.resize(numHiddenLayers + 2);
-            m_layers[i] = Matrix(hiddenLayerWidth, inputWidth + 1);
+            m_layers[0] = Layer(hiddenLayerWidth, inputWidth);
             for (size_t i = 0; i < numHiddenLayers; ++i)
             {
-                layers[i + 1] = Matrix(hiddenLayerWidth, hiddenLayerWidth + 1);
+                m_layers[i + 1] = Layer(hiddenLayerWidth, hiddenLayerWidth);
             }
-            layers.back() = Matrix(outputWidth, hiddenLayerWidth + 1);
+            m_layers.back() = Layer(outputWidth, hiddenLayerWidth);
 
             // Prealloc activation cache
-            m_activationCache.resize(2 + numHiddenLayers);
-            m_activationCache[0] = Matrix::Ones(inputWidth + 1, 1);
-            for (size_t i = 0; i < numHiddenLayers; ++i)
+            m_activationCache.resize(3 + numHiddenLayers);
+            m_activationCache[0] = Matrix::Ones(inputWidth, 1);
+            for (size_t i = 0; i < numHiddenLayers+1; ++i)
             {
-                m_activationCache[i + 1] = Matrix::Ones(hiddenLayerWidth + 1, 1);
+                m_activationCache[i + 1] = Matrix::Ones(hiddenLayerWidth, 1);
             }
+            m_activationCache.back() = Matrix::Ones(outputWidth, 1);
         }
 
-        Matrix forward(const Matrix& x)
+        const Matrix& forward(const Matrix& x)
         {
-            m_activationCache[0].TopRows(x.rows()) = x;
+            m_activationCache[0].topRows(x.rows()) = x;
             for (size_t i = 0; i < m_layers.size() - 1; ++i)
             {
                 // Convolution
-                m_activationCache[i + 1].TopRows(hiddenLayerWidth) = m_layers[i] * m_activationCache[i];
+                m_activationCache[i + 1] = m_layers[i].forward(m_activationCache[i]);
 
                 // Activation
                 m_activationCache[i + 1] = m_activationFn.forward(m_activationCache[i + 1]);
             }
 
             // final layer, with no activation function
-            return m_layers.back() * m_activationCache.back();
+            m_activationCache.back() = m_layers.back().forward(m_activationCache[m_layers.size()-1]);
+            return m_activationCache.back();
+        }
+        
+        void randomize(math::SquirrelRng& rng)
+        {
+            for (auto& layer : m_layers)
+                layer.randomize(rng);
+        }
+        
+        Matrix randomDelta(math::SquirrelRng& rng, float stepSize) const
+        {
+            size_t gradRows = 0;
+            size_t gradCols = 0;
+            for (auto& layer : m_layers)
+            {
+                gradRows += layer.gradRows();
+                gradCols = std::max(gradCols, layer.gradCols());
+            }
+
+            Matrix delta = Matrix::Zero(gradRows, gradCols);
+
+            auto row0 = 0;
+            for (auto& layer : m_layers)
+            {
+                delta.block(row0, 0, layer.gradRows(), layer.gradCols()) = layer.randomDelta(rng, stepSize);
+                row0 += layer.gradRows();
+            }
+
+            return delta;
+        }
+        
+        void step(Matrix delta)
+        {
+            auto row0 = 0;
+            for (auto& layer : m_layers)
+            {
+                layer.step(delta.block(row0, 0, layer.gradRows(), layer.gradCols()));
+                row0 += layer.gradRows();
+            }
         }
 
-    private:
         std::vector<Matrix> m_activationCache;
     };
-}
+} // namespace nn
--- ml/nn/layer.h
+++ ml/nn/layer.h
@@ -1,72 +1,54 @@
 #pragma once
 
-#include <Eigen/Eigen>
+#include <Eigen/Core>
 #include <math/noise.h>
+#include <cassert>
 
 namespace nn
 {
-	template<class S>
-	struct Layer
+	// Fully connected layer
+	struct FullyConnectedLayer
 	{
 		// Local types
-		using Scalar = S;
+		using Scalar = float;
 		using Matrix = Eigen::Matrix<Scalar, -1, -1>;
 
-		// The size of dW
-		virtual size_t gradRows() const = 0;
-		virtual size_t gradCols() const = 0;
-
-		// Forward evaluation
-		virtual Matrix forward(const Matrix& x) = 0;
-
-		// Back propagation
-		virtual Matrix d_dW(const Matrix& x) const = 0;
-		virtual Matrix d_dX(const Matrix& x) const = 0;
-
-		virtual void randomize(math::SquirrelRng& rng) = 0;
-		virtual void step(Scalar stepSize, const Matrix& dir) = 0;
-	};
-
-	// Fully connected layer
-	template<class S>
-	struct FullyConnectedLayer final : Layer<S>
-	{
 		size_t m, n;
 		Matrix W;
 
 		FullyConnectedLayer() = default;
-		FullyConnectedLayer(size_t _m, size_t _n) // Init as an (M x N) matrix
+		FullyConnectedLayer(size_t _m, size_t _n) // Init as an (M x N+1) matrix
 		{
 			m = _m;
 			n = _n;
-			W = Eigen::Matrix(m, n+1); // n+1 to store a bias term
+			W = Matrix(m, n+1); // n+1 to store a bias term
 		}
 
 		// The size of dW
-		size_t gradRows() const override { return m; }
-		size_t gradCols() const override { return n + 1; }
+		size_t gradRows() const { return m; }
+		size_t gradCols() const { return n + 1; }
 
 		// Forward evaluation
 		Matrix forward(const Matrix& x)
 		{
 			assert(W.cols() == x.rows() + 1);
-			m_xWithBias = Matrix::Ones(x.rows() + 1, x.cols());
-			m_X.topRows(x.rows()) = x;
+			m_xWithBias = Matrix::Ones(n + 1, 1);
+			m_xWithBias.topRows(n) = x;
 			return W * m_xWithBias;
 		}
 
 		// Back propagation
-		Matrix d_dX(const Matrix& x) override
+		Matrix d_dX(const Matrix& xWithBias)
 		{
 			return W.transpose();
 		}
 
-		Matrix d_dW(const Matrix& x) override
+		Matrix d_dW(const Matrix& xWithBias)
 		{
 			return m_xWithBias.transpose();
 		}
 
-		void randomize(math::SquirrelRng& rng) override
+		void randomize(math::SquirrelRng& rng)
 		{
 			auto norm = 1.f / W.cols();
 			for(auto i = 0; i < W.rows(); i++)
@@ -78,40 +60,59 @@ namespace nn
 			}
 		}
 
-		void step(Scalar stepSize, const Matrix& dir) override
+		Matrix randomDelta(math::SquirrelRng& rng, float stepSize) const
 		{
-			W += stepSize * dir;
+			Matrix delta = Matrix(m, n+1);
+
+			for (auto i = 0; i < W.rows(); i++)
+			{
+				for (auto j = 0; j < W.cols(); ++j)
+				{
+					delta(i, j) = rng.uniform(-stepSize, stepSize);
+				}
+			}
+
+			return delta;
+		}
+
+		void step(const Matrix& dir)
+		{
+			W += dir;
 		}
 
 	private:
 		Matrix m_xWithBias;
 	};
 
-	template<class S>
-	struct ReLu final : Layer<S>
+	struct ReLu
 	{
-		size_t gradRows() const override { return 0; }
-		size_t gradCols() const override { return 0; }
+		// Local types
+		using Scalar = float;
+		using Matrix = Eigen::Matrix<Scalar, -1, -1>;
 
-		Matrix forward(const Matrix& x) override {
+		Matrix forward(const Matrix& x)
+		{
 			return x.cwiseMax(Scalar(0));
 		}
 
-		Matrix d_dW(const Matrix& x, const Matrix& dz) const override {
+		Matrix d_dW(const Matrix& x, const Matrix& dz) const
+		{
 			return Matrix(0, 0); // Fixed function. No weights
 		}
 
-		Matrix d_dX(const Matrix& x) const {
+		Matrix d_dX(const Matrix& x) const
+		{
 			Matrix res(x.rows(), x.cols());
-			for (int i = 0; i < x.rows(); ++i) {
-				for (int j = 0; j < x.cols(); ++j) {
-					res(i, j) = x(i, j) == fwd(i, j) ? Scalar(1) : Scalar(0);
+
+			for (int i = 0; i < x.rows(); ++i)
+			{
+				for (int j = 0; j < x.cols(); ++j)
+				{
+					res(i, j) = (x(i, j) >= 0) ? Scalar(1) : Scalar(0);
 				}
 			}
+
 			return res;
 		}
-
-		void randomize() override {}
-		void step(Scalar, const Matrix&) override {}
 	};
 }
